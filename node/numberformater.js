0b02c01b3407ac24b99a9e78c1ad38fcc402482d
keshab sutar <keshabsingh981829@gmail.com>
2024-01-27 19:00:12 +0545

    Number formater concept

---
+++ node/numberformater.js
@@ -0,0 +1,11 @@
+//write a js function that puts comma in every thousand place
+//eg:10000=>10,000
+//1000=>1,000
+//1000000=>1,000,000
+
+const format = (N)=>{
+const newFormat = givenNumber.toLocalString("en-us"); 
+return newFormat;   
+};
+const res = format(1000);
+console.log(res);
