3b482fbc7e6e96eb2b454bacafbdb020729a24c1
keshab sutar <keshabsingh981829@gmail.com>
2024-01-27 19:07:15 +0545

    nodemailer concept

--- exercise/index.js
+++ exercise/index.js
@@ -1,12 +1,41 @@
+require("dotenv").config();
+
 const { sendMail } = require("./nodemailer");
-const mailFunction = async () => {
-  const result = await sendMail("Hello Keshab G");
+
+console.log(process.env.EMAIL, process.env.PASSWORD);
+const mailFunction = async (message) => {
+  const result = await sendMail(message);
   return result;
 };
 
-const getResult = async () => {
-  const res = await mailFunction();
-  console.log(res);
-};
+const message = {
+  from: "keshabsingh981829@gmail.com",
+  to: "sutarkeshab123@gmail.com",
+  subject: "nodemailer",
+  text: "Namaste",
+  html: '<h1>keshab</h1> Embedded image: <img src="cid:unique@nodemailer.com/">',
 
-getResult();
+  attachments: [
+    {
+      // utf-8 string as an attachment
+      filename: "text1.txt",
+      content: "hello world!",
+    },
+    {
+      path: "./my cover letter.pdf",
+    },
+    {
+      filename: "image.jpg",
+      path: "./keshabsutar.jpg",
+      cid: "unique@nodemailer.com",
+    },
+  ],
+};
+mailFunction(message);
+
+// const getResult = async (message) => {
+//   const res = await mailFunction(message);
+//   console.log(res);
+// };
+
+// getResult("hello keshab");
